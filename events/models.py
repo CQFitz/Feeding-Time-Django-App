4f7aec9ab787cd0e4e295e453824d440c849b8ba
CQFitz <nnabilmuqri123@gmail.com>
2021-09-06 11:12:20 +0000

    fix some mistake

--- events/models.py
+++ events/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Event(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
     event_name = models.CharField(max_length=100, blank=True, default='')
     event_description = models.CharField(max_length=100)
     other_details = models.CharField(max_length=100)
@@ -14,3 +15,5 @@ class Event(models.Model):
 
     def __str__(self):
         return self.event_name
+
+
