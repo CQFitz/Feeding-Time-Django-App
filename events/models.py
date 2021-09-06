9c5375f248222f9b08d9e9e5aa274160b6d74883
CQFitz <nnabilmuqri123@gmail.com>
2021-09-06 11:10:00 +0000

    create a basic model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    event_name = models.CharField(max_length=100, blank=True, default='')
+    event_description = models.CharField(max_length=100)
+    other_details = models.CharField(max_length=100)
+
+
+    class Meta:
+        ordering = ['created']
+
+
+    def __str__(self):
+        return self.event_name
